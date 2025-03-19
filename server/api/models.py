b1e2cfe8a1508dbe08411d62bd7fddae1d8531e6
Талимбек Бекзат <lokkitcdev@gmail.com>
2025-03-19 23:55:22 +0500

    Добавлен новый файл models.py с определением моделей UserRead и UserCreate для работы с пользователями. Реализована валидация полей name и surname с использованием регулярного выражения для проверки на наличие только букв.

---
+++ server/api/models.py
@@ -0,0 +1,34 @@
+import re
+import uuid
+from fastapi import HTTPException
+from pydantic import BaseModel, field_validator, EmailStr
+
+LETTER_MATCH_PATTERN = re.compile(r"^[а-яА-Яa-zA-Z]+$")
+
+
+
+class TunedModel(BaseModel):
+    class Config:
+        from_attributes = True
+
+
+class UserRead(TunedModel):
+    user_id: uuid.UUID
+    name: str
+    surname: str
+    email: EmailStr
+    is_active: bool
+
+class UserCreate(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+
+    @field_validator("name", "surname", mode="before")
+    def validate_name(cls, v):
+        if not LETTER_MATCH_PATTERN.match(v):
+            raise HTTPException(
+                status_code=422,
+                detail="Must contain only letters"
+            )
+        return v
