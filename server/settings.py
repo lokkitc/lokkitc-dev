31b20ad5bcfa64ea74c2bb3409f9ffc1f46f8a7d
Bekzat <talimbekbekzat@gmail.com>
2025-03-19 17:24:37 +0500

    Добавлен файл настроек сервера settings.py с конфигурацией для подключения к базе данных PostgreSQL.

---
+++ server/settings.py
@@ -0,0 +1,14 @@
+from envparse import Env
+
+env = Env()
+
+REAL_DATABASE_URL = env.str(
+    "REAL_DATABASE_URL",
+    default="postgresql+asyncpg://postgres:postgres@localhost:5434/postgres",
+)
+
+
+
+
+
+
