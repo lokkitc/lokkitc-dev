1bac81732b53474c22673a971bd5a92dfc499290
Талимбек Бекзат <lokkitcdev@gmail.com>
2025-03-19 20:30:11 +0500

    Добавлено сообщение о том, как остановить сервер, в файле main.py.

--- server/main.py
+++ server/main.py
@@ -111,4 +111,5 @@ app.include_router(main_api_router)
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
     print("\n\n\nServer is running on http://127.0.0.1:8000\n\n\n")
+    print("\n\n\nPress Ctrl+C to stop the server\n\n\n")
 
