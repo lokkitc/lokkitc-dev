2e71dbaf7a4bbce59e07253cc57f9836e51156cf
Bekzat <talimbekbekzat@gmail.com>
2025-03-19 18:45:49 +0500

    Добавлен класс User для работы с пользователями в базе данных, включая поля user_id, name, surname, email и is_active. Настроены асинхронные сессии и подключение к базе данных с использованием SQLAlchemy.

--- server/main.py
+++ server/main.py
@@ -9,7 +9,21 @@ import uuid
 import re
 from fastapi import HTTPException
 from typing import BaseModel, EmailStr, validator
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean
+
+engine = create_async_engine(settings.REAL_DATABASE_URL, echo=True, future=True)
+
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = "users"
 
-
+    user_id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(String)
+    surname: Mapped[str] = mapped_column(String)
+    email: Mapped[EmailStr] = mapped_column(EmailStr, unique=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
