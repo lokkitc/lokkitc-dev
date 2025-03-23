8d6e6d12327b8a41194703f9030f563c77bf99eb
Талимбек Бекзат <lokkitcdev@gmail.com>
2025-03-23 15:28:50 +0500

    Добавлены функции удаления и получения пользователя в классе UserDAL в файле dals.py. Обновлены импорты для поддержки новых функций.

--- server/db/dals.py
+++ server/db/dals.py
@@ -1,6 +1,8 @@
 from db.models import User
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from uuid import UUID
+from typing import Union
+from sqlalchemy import update, delete, select, and_
 
 class UserDAL:
     def __init__(self, db_session: AsyncSession):
@@ -16,4 +18,15 @@ class UserDAL:
         await self.db_session.flush()
         return new_user
     
-
+    async def delete_user(self, user_id: UUID) -> Union[User, None]:
+        query = update(User).where(and_(User.user_id == user_id, User.is_active == True)).values(is_active=False).returning(User.user_id)
+        result = await self.db_session.execute(query)
+        deleted_user_by_id = result.fetchone()
+        if deleted_user_by_id is not None:
+            return deleted_user_by_id[0]
+        return None
+    
+    async def get_user(self, user_id: UUID) -> Union[User, None]:
+        query = select(User).where(User.user_id == user_id)
+        result = await self.db_session.execute(query)
+        return result.scalar_one_or_none()
