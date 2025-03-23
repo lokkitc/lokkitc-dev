5d6d3a15eda2e26b15b19af59a7d5dfa6b917a88
Талимбек Бекзат <lokkitcdev@gmail.com>
2025-03-23 15:28:44 +0500

    Добавлена функция удаления пользователя с обработкой исключений в файле handlers.py. Обновлены импорты для новых моделей и добавлены соответствующие маршруты API.

--- server/api/handlers.py
+++ server/api/handlers.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.models import UserCreate, UserRead
+from api.models import UserCreate, UserRead, UserDeleteResponse, UpdateUserResponse
 from db.dals import UserDAL
 from db.session import get_db
+
+from fastapi import HTTPException
+from uuid import UUID
+from typing import Union
 
 user_router = APIRouter(prefix="/users", tags=["users"])
 
@@ -24,7 +28,24 @@ async def _create_new_user(body: UserCreate, db) -> UserRead:
                 is_active=new_user.is_active    
             )
         
+async def _delete_user(user_id, db) -> Union[UUID, None]:
+    async with db as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            deleted_user_id = await user_dal.delete_user(
+                user_id=user_id
+            )
+            return deleted_user_id
+
 
 @user_router.post("/", response_model=UserRead)
 async def create_user(body: UserCreate, db: AsyncSession = Depends(get_db)) -> UserRead:
     return await _create_new_user(body, db)
+
+@user_router.delete("/{user_id}", response_model=UserDeleteResponse)
+async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_db)) -> UserDeleteResponse:
+    deleted_user_id = await _delete_user(user_id, db)
+    if deleted_user_id is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return UserDeleteResponse(user_id=deleted_user_id)
+
