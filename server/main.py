319809ab798303eb3b1e97fd601a3636c8489a6b
Талимбек Бекзат <lokkitcdev@gmail.com>
2025-03-29 21:47:22 +0500

    Добавлен новый маршрутизатор для аутентификации пользователей с использованием login_router. Обновлен основной маршрутизатор для включения новых маршрутов с префиксами и тегами.

--- server/main.py
+++ server/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import uvicorn
 from fastapi.routing import APIRouter
 from api.routers.users import user_router
+from api.dependencies.auth import login_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="API для работы с базой данных", description="API для работы с базой данных")
@@ -16,7 +17,8 @@ app.add_middleware(
 
 main_router = APIRouter()
 main_router.include_router(user_router)
-app.include_router(main_router)
+app.include_router(main_router, prefix="/users", tags=["users"])
+app.include_router(login_router, prefix="/auth", tags=["auth"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
