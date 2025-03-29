1550b86e307df48f65a77e9ec5b35d6d07699e18
Талимбек Бекзат <lokkitcdev@gmail.com>
2025-03-29 21:46:20 +0500

    Добавлена функция создания токена доступа с использованием библиотеки jose. Реализована логика установки времени истечения токена на основе конфигурации. Удален класс Hasher и связанные с ним импорты.

--- server/core/security.py
+++ server/core/security.py
@@ -1,12 +1,22 @@
-from passlib.context import CryptContext
+from datetime import datetime
+from datetime import timedelta
+from typing import Optional
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from jose import jwt
 
-class Hasher:
-    @staticmethod
-    def get_password_hash(password: str) -> str:
-        return pwd_context.hash(password)
-    
-    @staticmethod
-    def verify_password(password: str, hashed_password: str) -> bool:
-        return pwd_context.verify(password, hashed_password)
+import core.config as settings
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
