090a78933ed16a28ded3f68cd0865653cd1a20fe
Талимбек Бекзат <lokkitcdev@gmail.com>
2025-04-02 11:20:54 +0500

    Добавлено новое правило проверки прав пользователя в user_service.py: реализована функция check_user_permissions для управления доступом к ресурсам. Также изменен путь импорта UserDAL и добавлена роль пользователя при создании нового пользователя.

--- server/api/services/user_service.py
+++ server/api/services/user_service.py
@@ -4,7 +4,8 @@ from typing import Union
 from sqlalchemy.ext.asyncio import AsyncSession
 from datetime import datetime
 from schemas.users import UserCreate, UserRead, UserUpdateRequest
-from db.dals import UserDAL
+from db.dals.user_dal import UserDAL
+from db.models.users import User, UserRole
 from core.hashing import Hasher
 
 async def create_new_user(body: UserCreate, session) -> UserRead:
@@ -15,6 +16,7 @@ async def create_new_user(body: UserCreate, session) -> UserRead:
             surname=body.surname,
             email=body.email,
             hashed_password=Hasher.get_password_hash(body.password),
+            roles=[UserRole.ROLE_USER,],
             username=body.username,
         )
         return UserRead(
@@ -108,6 +110,25 @@ async def get_users(session) -> list[UserRead]:
             updated_at=user.updated_at
             ) for user in users]
 
+def check_user_permissions(target_user: User, current_user: User) -> bool:
+     if target_user.user_id != current_user.user_id:
+         if not {
+             UserRole.ROLE_ADMIN,
+             UserRole.ROLE_SUPERADMIN,
+         }.intersection(current_user.roles):
+             return False
+         if (
+             UserRole.ROLE_SUPERADMIN in target_user.roles
+             and UserRole.ROLE_ADMIN in current_user.roles
+         ):
+             return False
+         if (
+             UserRole.ROLE_ADMIN in target_user.roles
+             and UserRole.ROLE_ADMIN in current_user.roles
+         ):
+             return False
+     return True
+
 async def update_user(updated_user_params: dict, user_id: UUID, session) -> Union[UUID, None]:
     async with session.begin():
         user_dal = UserDAL(session)
@@ -117,5 +138,4 @@ async def update_user(updated_user_params: dict, user_id: UUID, session) -> Unio
             **updated_user_params,
             updated_at=datetime.now()
         )
-        await session.commit() 
         return user_id
