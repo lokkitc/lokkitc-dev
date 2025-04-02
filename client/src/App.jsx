24dd0d2c11850090b3700e9694c7ba8f4ee9ee25
Талимбек Бекзат <lokkitcdev@gmail.com>
2025-04-02 11:19:25 +0500

    Добавлен новый маршрут для страницы авторизации в компонент App. Импортирован компонент Auth для обработки аутентификации пользователей.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Footer, BottomMobileNavbar } from './components/Base';
+import Auth from './pages/Auth/Auth';
 
 // Ленивая загрузка компонентов
 const Users = lazy(() => import('./pages/User/Users'));
 const Home = lazy(() => import('./pages/Home/Home'));
 const Offers = lazy(() => import('./pages/Offers'));
 const UserProfile = lazy(() => import('./pages/User/User-profile'));
+
 function App() {
   return (
     <Router>
@@ -19,6 +21,7 @@ function App() {
               <Route path="/users" element={<Users />} />
               <Route path="/offers" element={<Offers />} />
               <Route path="/users/:username" element={<UserProfile />} />
+              <Route path="/auth" element={<Auth />} />
             </Routes>
           </Suspense>
         </main>
